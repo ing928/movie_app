52f300fd1a7585ad179d70bcaf305340de7187c9
ing928 <78721578+ing928@users.noreply.github.com>
2021-03-07 15:30:49 +0900

    Css working

--- src/Movie.js
+++ src/Movie.js
@@ -8,13 +8,13 @@ function Movie({year,title,summary,poster,genre}){
             <img src={poster} alt={title} title={title}/>
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
+                <h5 className="movie__year">year: {year}</h5>
                 <ul className="movie__genre">
                     {genre.map((genre, index) =>(
                         <li key={index} className="genres__genre">{genre}</li>
                     ))}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0,130)}...</p>
             </div>
         </div>
     )
